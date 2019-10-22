0156b7f830b173a328799ed6c9c689b2387bcbba
Michael McGuire <michael3mcguire@gmail.com>
2019-10-22 15:13:32 -0400

    initial app

--- movies-app/client/src/App.js
+++ movies-app/client/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    movies: []
+  }
+  
+
+  async componentDidMount(){
+    let response = await axios.get("http://localhost:3000/movies");
+    let movies = response.data;
+    console.log(movies)
+    this.setState({movies})
+  }
+
+  render(){
+
+    const url = 'https://image.tmdb.org/t/p/w780/'
+
+    let movies  = this.state.movies.map((film, index)=>(
+      <div key={index}>
+        <h2>{film.title}</h2>
+        <p>{film.overview}</p>
+        <img src={ url + film.poster_path} alt="poster"/>
+        <img src={url + film.backdrop_path} alt="backdrop"/>
+      </div>
+    ))
+
+    return(
+      <div>{movies}</div>
+    )
+  }
+
 }
 
 export default App;
